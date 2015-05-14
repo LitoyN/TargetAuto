8f52a5887ad95593fddb23bbc816b3ae39cb73dd
LitoyN <litoynau@gmail.com>
2015-05-14 19:31:06 -0400

    Created Netbeans Project
    And that's it. Good work so far.

---
+++ main.cpp
@@ -0,0 +1,19 @@
+/* 
+ * File:   main.cpp
+ * Author: elliot
+ *
+ * Created on May 14, 2015, 7:29 PM
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
